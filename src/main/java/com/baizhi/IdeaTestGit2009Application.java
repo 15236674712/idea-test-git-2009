0b839ae9415d13485eeaaf873b9c9a0e6a15d398
zhangcn <15236674712@163.com>
2021-03-27 10:25:26 +0800

    这是项目第一次提交第一个版本

---
+++ src/main/java/com/baizhi/IdeaTestGit2009Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaTestGit2009Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaTestGit2009Application.class, args);
+    }
+
+}
