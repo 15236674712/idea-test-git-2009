0340313796249034ae0430a8991e9fc965bb2dd6
zhangcn <15236674712@163.com>
2021-03-27 10:35:52 +0800

    添加测试类IdeaTests

---
+++ src/test/java/com/baizhi/IdeaTestss.java
@@ -0,0 +1,16 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaTestss {
+
+    @Test
+    void contextLoads() {
+        System.out.println("====");
+        int a=10;
+        int b=20;
+    }
+
+}
